5cdf9d9e2317552848fbec146cc85fe9093eecfe
gram <jjangart5720@naver.com>
2025-05-19 20:58:57 +0900

    수정

---
+++ lib/screens/partnermanage/widget/wit_partnermanage_main_widget.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import '../../certificateholdermanage/wit_certificateholdermanage_detail_sc.dart';
+import '../../common/widget/wit_common_widget.dart';
+import '../../common/widget/wit_common_theme.dart';
+import '../../../util/wit_code_ut.dart';
+
+/**
+ * 협력업체 인증관리 리스트 뷰
+ */
+class PartnerListView extends StatelessWidget {
+  final List<dynamic> partnerList;
+  final Future<void> Function() getList;
+
+  const PartnerListView({
+    required this.partnerList,
+    required this.getList,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: partnerList.length,
+      itemBuilder: (context, index) {
+        final item = partnerList[index];
+        return PartnerCard(
+          item: item,
+          onTap: () async {
+            await Navigator.push(
+              context,
+              SlideRoute(page: CertificateHolderDetail(itemInfo: item)),
+            );
+            // 화면 복귀 시 리스트를 새로 조회
+            await getList();
+          },
+        );
+      },
+    );
+  }
+}
+
+/**
+ * 협력업체 인증관리 카드
+ */
+class PartnerCard extends StatefulWidget {
+
+  final dynamic item;
+  final VoidCallback onTap;
+
+  const PartnerCard({required this.item, required this.onTap});
+
+  @override
+  _PartnerCardState createState() => _PartnerCardState();
+}
+
+class _PartnerCardState extends State<PartnerCard> {
+
+  Color _backgroundColor = WitCommonTheme.wit_white; // 초기 배경 색상
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) {
+        setState(() {
+          _backgroundColor = WitCommonTheme.wit_extraLightGrey;
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          _backgroundColor = WitCommonTheme.wit_white;
+        });
+      },
+      onTapCancel: () {
+        setState(() {
+          _backgroundColor = WitCommonTheme.wit_white;
+        });
+      },
+      onTap: widget.onTap,
+      child: Container(
+        color: _backgroundColor,
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Row(
+              children: [
+                SizedBox(width: 20),
+                Column(
+                  children: [
+                    // 이미지가 존재할 경우에만 표시
+                    if (widget.item["storeImage"] != null) ...[
+                      Container(
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: WitCommonTheme.wit_gray.withOpacity(0.5),
+                            width: 1, // 테두리 두께
+                          ),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            apiUrl + widget.item["storeImage"],
+                            width: 70,
+                            height: 70,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                    if (widget.item["storeImage"] == null) ...[
+                      Container(
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          color: WitCommonTheme.wit_extraLightGrey,
+                          borderRadius: BorderRadius.circular(5),
+                          border: Border.all(
+                            color: WitCommonTheme.wit_extraLightGrey,
+                            width: 1,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            widget.item["storeName"],
+                            style: WitCommonTheme.title,
+                          ),
+                          if (widget.item["certificationYn"] == "Y")
+                            Padding(
+                              padding: const EdgeInsets.only(left: 6.0),
+                              child: Icon(
+                                Icons.emoji_events,
+                                size: 20,
+                                color: Colors.amber,
+                              ),
+                            ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Row(
+                              children: [
+                                // 인증 여부를 표시할 네모 박스
+                                Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                                  decoration: BoxDecoration(
+                                    color: WitCommonTheme.wit_lightGreen,
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  child: Text(
+                                    "협력업체 여부",
+                                    style: WitCommonTheme.caption.copyWith(color: WitCommonTheme.wit_white),
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                Text(
+                                    "${widget.item["certificationNm"] ?? "협력업체 미인증"}",
+                                    style: WitCommonTheme.caption
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 1,
+              color:WitCommonTheme.wit_extraLightGrey,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
--- lib/screens/partnermanage/wit_partnermanage_main_sc.dart
+++ lib/screens/partnermanage/wit_partnermanage_main_sc.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import '../partnermanage/widget/wit_partnermanage_main_widget.dart';
+import '../../util/wit_api_ut.dart';
+import '../common/widget/wit_common_theme.dart';
+import '../common/widget/wit_common_widget.dart';
 
 /**
  * 협력업체 인증관리 메인
@@ -20,12 +26,24 @@ class PartnerManage extends StatefulWidget {
  */
 class PartnerManageState extends State<PartnerManage> {
 
+  // 협력업체 리스트
+  List<dynamic> partnerList = [];
+  // 검색 활성화 여부
+  bool isSearching = false;
+  // TextEdit 컨트롤러
+  TextEditingController searchController = TextEditingController();
+  // 드랍박스 선택한 상태값
+  String? dropBoxSelectStat = "";
+  
   /**
    * 화면 초기화
    */
   @override
   void initState() {
     super.initState();
+
+    // 협력업체 인증 내역 조회
+    getPartnerList();
   }
 
   /**
@@ -33,11 +51,149 @@ class PartnerManageState extends State<PartnerManage> {
    */
   @override
   Widget build(BuildContext context) {
+
+    final Map<String, String> _functionOptionsMap = {
+      "": '전체',
+      "N": '협력업체 미인증',
+      "Y": '협력업체 인증',
+    };
+
+    final List<DropdownMenuItem<String>> _dropdownItems = _functionOptionsMap.entries.map((entry) {
+      return DropdownMenuItem<String>(
+        value: entry.key,
+        child: Text(entry.value),
+      );
+    }).toList();
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("협력업체 인증관리"),
+      appBar: SearchAppBar(
+        appBarTitle: "협력업체 인증 관리",
+        isSearching: isSearching,
+        searchController: searchController,
+        onSearchToggle: () {
+          if (isSearching) {
+            stopSearch();
+          } else {
+            startSearch();
+          }
+        },
+        onSearchSubmit: (value) => filterList(),
+      ),
+      body: SafeArea(
+        child: Column( // 여러 위젯을 세로로 배열하기 위해 Column 추가
+          children: [
+            Container(
+              color: WitCommonTheme.wit_white, // 배경색을 흰색으로 설정
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                child: InputDecorator(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 0.0),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      dropdownColor: WitCommonTheme.wit_white,
+                      value: dropBoxSelectStat,
+                      items: _dropdownItems,
+                      onChanged: (String? newValue) {
+                        if (newValue != null) {
+                          setState(() {
+                            dropBoxSelectStat = newValue;
+                            // 협력업체 인증 내역 조회
+                            getPartnerList();
+                          });
+                        }
+                      },
+                      style: WitCommonTheme.subtitle,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 1,
+              color: WitCommonTheme.wit_lightgray,
+            ),
+            Expanded(
+              child: partnerList.isEmpty
+                  ? Container(
+                color: WitCommonTheme.wit_white,
+                child: Center(
+                  child: Text(
+                    "조회된 데이터가 없습니다.",
+                    style: WitCommonTheme.title,
+                  ),
+                ),
+              )
+                  : PartnerListView(
+                partnerList: partnerList,
+                getList: getPartnerList, // 협력업체 인증 내역 조회
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  // [서비스] 협력업체 인증 내역 조회
+  Future<void> getPartnerList() async {
+
+    // 데이터 초기화
+    partnerList = [];
+
+    // REST ID
+    String restId = "getSellerList";
+
+    print(dropBoxSelectStat);
+
+    // PARAM
+    final param = jsonEncode({
+      "storeName" : searchController.text,
+      "certificationYn" : "Y",
+    });
+
+    // API 호출 (협력업체 인증 내역 조회)
+    final _partnerList = await sendPostRequest(restId, param);
+
+    print(_partnerList.length);
+
+    // 결과 셋팅
+    setState(() {
+      partnerList.addAll(_partnerList);
+    });
+  }
+
+  /**
+   * [이벤트] 검색 기능 활성화
+   */
+  void startSearch() {
+    setState(() {
+      isSearching = true;
+    });
+  }
+
+  /**
+   * [이벤트] 검색 기능 비활성화
+   */
+  void stopSearch() {
+    setState(() {
+      isSearching = false;
+      searchController.clear();
+    });
+    // 검색 완료시 필터 제거
+    getPartnerList();
+  }
+
+  /**
+   * [이벤트] 필터 검색
+   */
+  void filterList() {
+    // 필터 검색
+    getPartnerList();
   }
 }
